a11ef7b15c1a2bcebab22f565151d555c1c02b27
Thomas Eberle <thomas.eberle@technobase.fm>
2015-07-18 22:25:57 +0200

    #69 Multiprocessing eingefügt. Requests wird in mehrere Threads gesplittet, Abarbeitung ist somit wesentlich schneller.

--- telegram/bot/TelegramBotStarter.py
+++ telegram/bot/TelegramBotStarter.py
@@ -4,10 +4,11 @@ path = '/usr/tgbot/'
 sys.path.append(path)
 
 from telegram.bot.tglogging.TGLogger import logger
+from telegram.bot.server.telegrambothttpsserver import start_pipe
 
 
 from telegram.bot.server.telegrambothttpsserver import start
 
 if __name__ == '__main__':
     logger.info("Main method initiated.")
-    start()
+    start_pipe()
--- telegram/bot/server/telegrambothttpsserver.py
+++ telegram/bot/server/telegrambothttpsserver.py
@@ -1,7 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import socketserver
 import ssl
+import os
+import sys
 import json
+from multiprocessing import freeze_support, Process
 from telegram.bot.weareone.common.weareonetgbot import WeAreOneBot
 from telegram.bot.config.tgbotconfigparser import TGBotConfigParser
 from telegram.bot.tglogging.TGLogger import logger
@@ -9,12 +12,11 @@ from telegram.bot.tglogging.TGLogger import logger
 config = TGBotConfigParser("config.ini")
 conf = config.load()
 
-
 class ForkingHTTPServer(socketserver.ForkingMixIn, HTTPServer):
     def finish_request(self, request, client_address):
-        request.settimeout(30)
-        logger.debug("Request garbaged.")
+        request.settimeout(10)
         HTTPServer.finish_request(self, request, client_address)
+        logger.debug("Request garbaged.")
 
 
 class RequestHandler(BaseHTTPRequestHandler):
@@ -26,13 +28,21 @@ class RequestHandler(BaseHTTPRequestHandler):
         sentMessage = postvar.decode("UTF-8")
         data = json.loads(sentMessage)
         WeAreOneBot.activateBot(data)
-        logger.debug("Message from @"+str(data["message"]["from"]["username"])+": \""+str(data["message"]["text"])+"\"")
-        return None
+        logger.debug(
+            "Message from @" + str(data["message"]["from"]["username"]) + ": \"" + str(data["message"]["text"]) + "\"")
+        self.handle()
 
 
-def start():
+def serve_forever(server):
+    logger.info('Starting server.')
     try:
-        serveraddr = (conf.get("basics", "address"), int(conf.get("basics", "port")))
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+
+def start(serveraddr, number_of_processes):
+    try:
         srvr = ForkingHTTPServer(serveraddr, RequestHandler)
         logger.info("Server initialised.")
 
@@ -41,13 +51,28 @@ def start():
                                       ca_certs=conf.get("ssl", "ca_certs"),
                                       server_side=True)
         logger.info("SSL Data inserted.")
-        logger.info("Server up!")
-        srvr.serve_forever()  # serve_forever
+        for i in range(number_of_processes - 1):
+            Process(target=serve_forever, args=(srvr,)).start()
+
+        serve_forever(srvr)  # serve_forever
 
     except KeyboardInterrupt:
         logger.info("Server will shut down...")
         srvr.socket.close()
 
 
+def start_pipe():
+    DIR = os.path.join(os.path.dirname(__file__), '..')
+    serveraddr = (conf.get("basics", "address"), int(conf.get("basics", "port")))
+    processes = 10
+
+    logger.info('Serving at http://%s:%d using %d worker processes' % \
+                (serveraddr[0], serveraddr[1], processes))
+
+    os.chdir(DIR)
+    start(serveraddr, processes)
+
+
 if __name__ == '__main__':
-    start()
+    freeze_support()
+    start_pipe()
