153fca8e7bcae5cf5d450a5f827bf0de9973be02
Thomas Eberle <Thomas@tommyelroy.com>
2015-12-27 13:14:28 +0100

    ImportError behoben

--- telegram/config/tgbotconfigparser.py
+++ telegram/config/tgbotconfigparser.py
@@ -3,6 +3,7 @@ __author__ = 'Thomas Eberle'
 
 import configparser
 import pkg_resources
+import resources.config
 
 
 class TGBotConfigParser:
@@ -24,12 +25,3 @@ class TGBotConfigParser:
         configdata = pkg_resources.resource_filename("resources.config", self.filename)
         self.config.read_file(open(configdata, encoding="latin-1"), source=self.filename)
         return self.config
-
-
-if __name__ == '__main__':
-    print(dir)
-    config = TGBotConfigParser("config.ini")
-    data = config.load()
-    print(data.get("basics", "address"))
-    print(config.config.get("tgapi", "bot_link"))
-    print(data.get("tgapi", "bot_link"))
